af07e9742f2ca92f00274e65b396ef9a13785de0
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-03 15:18:22 +0200

    style(lotto): fix style regarding linter

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -3,16 +3,15 @@ package lotto
 private const val TICKET_COST = 1000
 
 class LottoMachine(val money: Money) {
-
     val tickets: List<Lotto> = issueTickets()
 
     fun issueTickets(): List<Lotto> {
         val ticketCount: Int = money.getMoneyAmount() / TICKET_COST
 
-        return (1 .. ticketCount).map {
+        return (1..ticketCount).map {
             Lotto(generateRandomNumber())
         }
     }
 
-    private fun generateRandomNumber() = (1 .. 45).shuffled().take(6).sorted();
+    private fun generateRandomNumber() = (1..45).shuffled().take(6).sorted()
 }
