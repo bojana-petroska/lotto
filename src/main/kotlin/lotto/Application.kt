fdfbc9ecf58221817035fd5b3f8f9f0efeba5ebd
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-03 16:50:07 +0200

    feat(lotto): implement game flow

---
+++ src/main/kotlin/lotto/Application.kt
@@ -0,0 +1,24 @@
+package lotto
+
+import view.InputView
+import view.ResultView
+
+fun main() {
+    val amountInput = InputView.getAmountInput()
+    val purchaseAmount = Money(amountInput)
+    val lottoMachine = LottoMachine(purchaseAmount)
+    val lottoTickets = lottoMachine.tickets
+
+    ResultView.viewTicketsAmount(lottoTickets.size)
+    ResultView.viewTickets(lottoTickets)
+
+    val winningNumbers = Lotto(InputView.getWinningNumbers())
+    val bonusNumber = InputView.getBonusNumber()
+
+    val game = LottoGame(lottoTickets, winningNumbers, bonusNumber)
+
+    game.play()
+
+    ResultView.viewWinning(game.results)
+    ResultView.viewRate(game.winningAmount, purchaseAmount.amount)
+}
