f550eb1dcbdd53dc1ac2ac65427f111024a6598c
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-07 17:49:35 +0200

    feat(lotto): add display of the manual tickets

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,11 +6,23 @@ import view.ResultView
 fun main() {
     val amountInput = InputView.getAmountInput()
     val purchaseAmount = Money.of(amountInput)
-    val lottoMachine = LottoMachine(purchaseAmount)
+
+    val maxTickets = purchaseAmount.amount / TICKET_COST
+    val manuelTicketCount = InputView.getManualTicketCount(maxTickets)
+
+    val allManualTickets = mutableListOf<LottoNumbers>()
+    InputView.getInputForNumber()
+    repeat(manuelTicketCount) {
+        val singleManualTicketNumbers = InputView.getManualNumbers()
+        allManualTickets.add(singleManualTicketNumbers)
+    }
+
+    val lottoMachine = LottoMachine(purchaseAmount, allManualTickets)
     val lottoTickets = lottoMachine.tickets
 
+    val automatedTicketCount = lottoTickets.size - manuelTicketCount
     ResultView.viewTicketsAmount(lottoTickets.size)
-    ResultView.viewTickets(lottoTickets)
+    ResultView.viewTickets(lottoTickets, manuelTicketCount, automatedTicketCount)
 
     val winningNumbersList = InputView.getWinningNumbers()
     val winningNumbers = LottoNumbers.of(winningNumbersList)
