46f3b8496d6209d1909ce5893b8dde3a2b02ba00
Bojana Petroska <bojana.petroska@yahoo.com>
2025-07-12 18:11:31 +0200

    refactor(lotto): remove main logic by introducing LottoController and PurchaseManager

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,41 +1,5 @@
 package lotto
 
-import view.InputView
-import view.ResultView
-
 fun main() {
-    val amountInput = InputView.getAmountInput()
-    val purchaseAmount = Money.of(amountInput)
-
-    val maxTickets = purchaseAmount.amount / TICKET_COST
-    val manuelTicketCount = InputView.getManualTicketCount(maxTickets)
-
-    val allManualTickets = mutableListOf<LottoNumbers>()
-    InputView.getInputForNumber()
-    repeat(manuelTicketCount) {
-        val singleManualTicketNumbers = InputView.getManualNumbers()
-        allManualTickets.add(singleManualTicketNumbers)
-    }
-
-    val lottoMachine = LottoMachine(purchaseAmount, allManualTickets)
-    val lottoTickets = lottoMachine.tickets
-
-    val automatedTicketCount = lottoTickets.size - manuelTicketCount
-    ResultView.viewTicketsAmount(lottoTickets.size)
-    ResultView.viewTickets(lottoTickets, manuelTicketCount, automatedTicketCount)
-
-    val winningNumbersList = InputView.getWinningNumbers()
-    val winningNumbers = LottoNumbers.of(winningNumbersList)
-
-    val bonusNumberInput = InputView.getBonusNumber()
-    val bonusNumber = BonusNumber.of(bonusNumberInput, winningNumbers.numbers)
-
-    val winningLotto = Lotto(winningNumbers)
-
-    val game = LottoGame(lottoTickets, winningLotto, bonusNumber)
-
-    game.play()
-
-    ResultView.viewWinning(game.results)
-    ResultView.viewRate(game.winningAmount, purchaseAmount.amount)
+    LottoController.start()
 }
